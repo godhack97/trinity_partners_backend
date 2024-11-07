c02c494d896cf182d6d8945cd4df9d3bfa89651b
Андрей Рик 🔥 fullstack-freelancer.com <contact@fullstack-freelancer.com>
2024-11-07 11:34:56 +0300

    UserInfo -> UserInfoEntity

--- src/orm/entities/user.entity.ts
+++ src/orm/entities/user.entity.ts
@@ -11,7 +11,6 @@ import { BasisEntity } from "./basis.entity";
 import { CompanyEmployeeEntity } from "./company-employee.entity";
 import { CompanyEntity } from "@orm/entities/company.entity";
 import { UserInfoEntity } from "@orm/entities/user-info.entity";
-import { UserInfo } from ".";
 
 @Entity({
   name: "users"
@@ -35,8 +34,8 @@ export class UserEntity extends BasisEntity {
   @OneToOne(() => CompanyEmployeeEntity, (CompanyEmployee) => CompanyEmployee.employee)
   company_employee: CompanyEmployeeEntity;
 
-  @OneToOne(() => UserInfo, (userInfo: UserInfo) => userInfo.user) // Добавляем связь с UserInfo
-  user_info: UserInfo;
+  @OneToOne(() => UserInfoEntity, (userInfo: UserInfoEntity) => userInfo.user) // Добавляем связь с UserInfoEntity
+  user_info: UserInfoEntity;
 
   @ManyToOne(() => RoleEntity, (role: RoleEntity) => role.id, {eager: true})
   @JoinColumn({ name: "role_id" })
