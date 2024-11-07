48e94fe30eff4af503a7410656f36d5a459d86f7
Андрей Рик 🔥 fullstack-freelancer.com <contact@fullstack-freelancer.com>
2024-11-07 11:51:11 +0300

    Актуальные изменения

--- src/api/deal/dto/request/search-deal.dto.ts
+++ src/api/deal/dto/request/search-deal.dto.ts
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import { IsDateRu, IsEnumRu } from "@decorators/validate";
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { DealStatus } from "@orm/entities";
 import { Type } from "class-transformer";
 import { IsEnum, IsOptional } from "class-validator";
-=======
-import { IsDateRu } from "@decorators/validate";
-import { ApiProperty } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { IsOptional } from "class-validator";
->>>>>>> 58ca369 (Добавление роутов выдачи сделок)
 
 export class SearchDealDto {
 
@@ -26,7 +19,6 @@ export class SearchDealDto {
   @Type(() => Date)
   @ApiPropertyOptional()
   endDate?: Date;
-<<<<<<< HEAD
 
   @ApiProperty()
   @IsEnum(DealStatus, { each: true })
@@ -38,6 +30,4 @@ export class SearchDealDto {
   @IsOptional()
   @ApiPropertyOptional()
   search?: string;
-=======
->>>>>>> 58ca369 (Добавление роутов выдачи сделок)
 }
