068c125f4e5489891da286d4144f1158946a6057
Андрей Рик 🔥 fullstack-freelancer.com <contact@fullstack-freelancer.com>
2024-11-13 02:21:25 +0300

    FIX: Активация аккаунта сотрудника при одобрении из ЛК партнёра

--- src/api/company/company.service.ts
+++ src/api/company/company.service.ts
@@ -20,6 +20,7 @@ export class CompanyService {
   }
 
   async addEmplyee(request: Request, addEmployeeDto: AddEmployeeRequestDto) {
+
     const token = this.authTokenService.extractToken(request);
     const role =  await this.authTokenService.getUserRole(token);
 
@@ -27,15 +28,7 @@ export class CompanyService {
       throw new HttpException('У вас нет прав для данного действия', HttpStatus.FORBIDDEN);
     }
 
-    //Получаем пользователя с сущностью CompanyEmployees и проверяем его статус, если он отклонен или в ожидании то добовляем его в сотрудники
-    const user =  await this.userRepository.findByEmailWithCompanyEmployees(addEmployeeDto.email);
-   
-   
-
-    if(!user || ![CompanyEmployeeStatus.Pending, CompanyEmployeeStatus.Reject].includes(user.company_employee.status) || user.role.name !==RoleTypes.Employee) {
-      return { succes: false }
-    }
-
+    const user = await this.userRepository.findByEmailWithCompanyEmployees(addEmployeeDto.email);
     
     const companyId =  (await this.companyEmployeeRepository.findCompanyEmployeeByEmployeeId(role.userId)).company_id;
 
@@ -43,18 +36,20 @@ export class CompanyService {
 
     await this.userInfoRepository.update(user.user_info.id, {
       company_name: companyName,
-    })
+    });
     
-    // В текущей реализации перезаписываем company_id если оно было другое. Так как при создании пользователя это никак не проверятется
-    await this.companyEmployeeRepository.update(user.company_employee.id, {
+    const employeRelationId = ( await this.companyEmployeeRepository.findCompanyEmployeeByEmployeeId(user.id) ).id;
+    
+    await this.companyEmployeeRepository.update( employeRelationId, {
+
       company_id: companyId,
       employee_id: user.id,
       status: CompanyEmployeeStatus.Accept
+
     });
-    //return user;
-    // return await this.companyRepository.find({
-    //   relations: ['employee']
-    //   })
+
+    await this.userRepository.update(user.id, { is_activated: true });
+    
     return { succes: true }
   }
 
