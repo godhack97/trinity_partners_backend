d76a3a64ed0eec608d664c78316276d6a1a1bddf
djentleman <godforger@yandex.ru>
2024-12-20 19:42:37 +0300

    add user setting entity

--- src/orm/entities/index.ts
+++ src/orm/entities/index.ts
@@ -2,6 +2,7 @@ export * from 'src/orm/entities/company.entity';
 export * from 'src/orm/entities/company-employee.entity';
 export * from 'src/orm/entities/reset-token.entity';
 export * from 'src/orm/entities/role.entity';
+export * from 'src/orm/entities/user-setting.entity';
 export * from 'src/orm/entities/user-info.entity';
 export * from 'src/orm/entities/user.entity';
 export * from 'src/orm/entities/deal.entity';
---
+++ src/orm/repositories/user-setting.repository.ts
@@ -0,0 +1,17 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  UserInfoEntity,
+  UserSettingEntity
+} from "@orm/entities";
+
+@Injectable()
+export class UserSettingRepository extends Repository<UserSettingEntity> {
+  constructor(
+    @InjectRepository(UserSettingEntity)
+    private repo: Repository<UserSettingEntity>,
+  ) {
+    super(repo.target, repo.manager, repo.queryRunner);
+  }
+}
