edb17d16bb4adaa77f074d074855eb3b1eb549d9
djentleman <godforger@yandex.ru>
2025-02-04 14:07:17 +0300

    дто для уведомлений

---
+++ src/api/notification/notifications.response.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from "@nestjs/swagger";
+import {
+  NotificationIconType,
+} from "@orm/entities";
+import { Expose } from "class-transformer";
+
+export class NotificationsResponseDto {
+  @ApiProperty()
+  @Expose()
+  id: number;
+
+  @ApiProperty()
+  @Expose()
+  title: string;
+
+  @ApiProperty()
+  @Expose()
+  text: string;
+
+  @ApiProperty()
+  @Expose()
+  is_read: boolean;
+
+  @ApiProperty()
+  @Expose()
+  icon: NotificationIconType;
+
+  @ApiProperty()
+  @Expose()
+  created_at: string;
+}
