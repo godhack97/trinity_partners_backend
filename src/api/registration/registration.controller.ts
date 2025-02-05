ba347ed676367497c571a92e567f2e7d4eb573b9
djentleman <godforger@yandex.ru>
2025-02-05 08:11:30 +0300

    поменял типа дто при регистрации суперадмина, отделил создания админа от проверки секрета

--- src/api/registration/registration.controller.ts
+++ src/api/registration/registration.controller.ts
@@ -4,7 +4,9 @@ import { Public } from 'src/decorators/Public';
 import { RegistrationEmployeeRequestDto } from './dto/request/registration-employee.request.dto';
 import { RegistrationCompanyRequestDto } from './dto/request/registration-company.request.dto';
 import { RegistrationService } from './registration.service';
-import { RegistrationSuperAdminDto } from "./dto/request/registration-super-admin.request.dto";
+import {
+  RegistrationSuperAdminWithSecretDto
+} from "./dto/request/registration-super-admin.request.dto";
 
 @ApiTags('registration')
 @Controller('registration')
@@ -30,7 +32,7 @@ export class RegistrationController {
 
   @Public()
   @Post('/super-admin')
-  createSuperAdmin(@Body() registrationSuperAdminDto: RegistrationSuperAdminDto) {
-    return this.registrationService.createSuperAdmin(registrationSuperAdminDto);
+  createSuperAdmin(@Body() registrationSuperAdminDto: RegistrationSuperAdminWithSecretDto) {
+    return this.registrationService.createSuperAdminWithSecret(registrationSuperAdminDto);
   }
 }
