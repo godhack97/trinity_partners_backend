559d4b799e17680f0ffd9e87c7c7291dca6835e4
djentleman <godforger@yandex.ru>
2025-02-05 08:06:51 +0300

    импорт зависимостей

--- src/api/admin/user/admin-user.module.ts
+++ src/api/admin/user/admin-user.module.ts
@@ -1,10 +1,16 @@
+import { EmailConfirmerService } from "@api/email-confirmer/email-confirmer.service";
+import { UserService } from "@api/user/user.service";
 import { Module } from '@nestjs/common';
 import { AdminUserService } from './admin-user.service';
 import { AdminUserController } from './admin-user.controller';
 
 @Module({
   controllers: [AdminUserController],
-  providers: [AdminUserService],
+  providers: [
+    AdminUserService,
+    UserService,
+    EmailConfirmerService
+  ],
   exports: [AdminUserService],
 })
 export class AdminUserModule { }
