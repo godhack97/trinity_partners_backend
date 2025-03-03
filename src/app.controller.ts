ac83563648e49e38bcdab9fec470a2165b5669fe
djentleman <godforger@yandex.ru>
2025-03-03 17:26:47 +0300

    отставил для дебага

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { HbsViewService } from "@app/hbs-view/hbs-view.service";
+import { SendsayService } from "@app/sendsay/sendsay.service";
 import { MailerService } from "@nestjs-modules/mailer";
 import { Controller, Get } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
@@ -12,12 +13,44 @@ export class AppController {
     private readonly configService: ConfigService,
     private readonly hbsViewService: HbsViewService,
     private readonly mailerService: MailerService,
+    private readonly sendsayService: SendsayService,
 
   ) {}
 
   @Public()
   @Get()
   async getHello() {
+    // await this.mailerService.sendMail({
+    //   to: 'godforger@yandex.ru',
+    //   subject: 'ntcn',
+    //   template: 'registration-employee--img-as-base64.hbs',
+    //   context: {
+    //     link: 'asdasda',
+    //        URL: 'partner.trinity.ru'
+    //   }
+    // })
+
+    // try {
+    //   let to = 'rrikkster@gmail.com';
+    //   let to1 = 'godforger@yandex.ru';
+    //   const data: any = await this.sendsayService.sendMail({
+    //     to: to,
+    //     subject: 'проверка связи1',
+    //     template: 'registration-employee--img-as-url.hbs',
+    //     context: {
+    //       link: 'asdasda',
+    //       URL: 'partner.trinity.ru'
+    //     }
+    //   })
+    //   console.log({
+    //     data
+    //   }, data.errors)
+    // } catch (error) {
+    //   console.log({
+    //     error
+    //   })
+    // }
+
     return 'Hello server!';
   }
 }
