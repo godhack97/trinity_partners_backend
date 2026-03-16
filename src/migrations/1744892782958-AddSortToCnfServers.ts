628ebe39baf4df88c4eeebdfe29e4c813db9abe9
Alexander Rizel <ar@outsi.de>
2026-03-16 15:02:57 +0300

    fix: add timestamp to AddSortToCnfServers migration class name

--- src/migrations/1744892782958-AddSortToCnfServers.ts
+++ src/migrations/1744892782958-AddSortToCnfServers.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddSortToCnfServers implements MigrationInterface {
+export class AddSortToCnfServers1744892782958 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE cnf_servers ADD sort INT NOT NULL DEFAULT 100`,
