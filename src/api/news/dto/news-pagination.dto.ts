26e926efba7273b47965229dcf8a8562414f4832
Alexander Rizel <ar@outsi.de>
2026-06-28 12:04:44 +0400

    Parse news pagination query params

--- src/api/news/dto/news-pagination.dto.ts
+++ src/api/news/dto/news-pagination.dto.ts
@@ -1,14 +1,19 @@
 import { MinRu } from "@decorators/validate";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional } from "class-validator";
+import { Type } from "class-transformer";
+import { IsInt, IsOptional } from "class-validator";
 
 export class NewsPaginationDto {
   @ApiProperty()
+  @Type(() => Number)
+  @IsInt()
   @MinRu(1)
   @IsOptional()
   page: number;
 
   @ApiProperty()
+  @Type(() => Number)
+  @IsInt()
   @MinRu(1)
   @IsOptional()
   limit: number;
