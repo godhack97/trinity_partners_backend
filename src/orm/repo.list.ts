b5e1ccea5443909323180225ad42b9e53af849e1
Alexander Rizel <ar@outsi.de>
2026-08-12 17:32:07 +0400

    bug fix

--- src/orm/repo.list.ts
+++ src/orm/repo.list.ts
@@ -1,11 +1,5 @@
 import * as repositories from "./repositories";
 
-export const repoList = createRepos();
-
-function createRepos(): any[] {
-  const repoList = [];
-  for (const entity of Object.values(repositories)) {
-    repoList.push(entity);
-  }
-  return repoList;
-}
+export const repoList = Object.values(repositories).filter(
+  (repository) => typeof repository === "function"
+);
